3fb9d27d12419a3e148826d266d5e1f0030b39a8
ADITYA DHANANJAY PAGAR <pagaraditya01@gmail.com>
2025-12-15 22:15:02 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,92 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "7cab9698-534e-4f4b-88c1-a81d04311a69",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Welcome to the Personal Introduction Program!\n",
+      "\n"
+     ]
+    },
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "What is your name? Aditya\n",
+      "How old are you? 23\n",
+      "Which city are you from? Nashik\n",
+      "What is your favorite hobby? Playing Cricket\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "\n",
+      " Welcome Aditya\n",
+      "You are 23 years old.\n",
+      "You live in Nashik.\n",
+      "And you love Playing Cricket.\n",
+      "\n",
+      "Thank you!\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Personal Introduction Program\n",
+    "# Week 1 - Python Basics Internship Task\n",
+    "\n",
+    "print(\"Welcome to the Personal Introduction Program!\\n\")\n",
+    "\n",
+    "# Taking user input\n",
+    "name = input(\"What is your name?\")\n",
+    "age = input(\"How old are you?\")\n",
+    "city = input(\"Which city are you from?\")\n",
+    "hobby = input(\"What is your favorite hobby?\")\n",
+    "\n",
+    "# Displaying output\n",
+    "print(\"\\n Welcome\", name)\n",
+    "print(f\"You are {age} years old.\")\n",
+    "print(f\"You live in {city}.\")\n",
+    "print(f\"And you love {hobby}.\")\n",
+    "\n",
+    "print(\"\\nThank you!\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d1f92b0a-fad9-4822-8c5e-5f99cc071b69",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
